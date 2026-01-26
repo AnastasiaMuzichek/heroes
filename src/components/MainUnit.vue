22c57841127a1d14779827cd67139698fc768fc3
Anastasia <anastasiamyzichek1997@gmail.com>
2026-01-26 18:08:05 +0400

    второй коммит

--- src/components/MainUnit.vue
+++ src/components/MainUnit.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
-import NameHero from './NameHero.vue';
 
-const props = defineProps({
+defineProps({
     fullImage: String,
 });
 </script>
 
 <template>
-    <NameHero></NameHero>
     <div class="main-page">
         <img :src="fullImage" ></img>
-       
     </div>
-    
 </template>
 
 <style scoped>
@@ -21,5 +17,4 @@ const props = defineProps({
     display: flex;
     justify-content: center;
 } 
-
 </style>
