66a040363115c5866eba2c6e2d4b76e22c1918bc
Anastasia <anastasiamyzichek1997@gmail.com>
2026-06-26 21:29:33 +0400

    main: добавлен плагин для копирования статики

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
 export function getImageCity(name: string) {
-    return new URL('/images/cities/' + name + '.webp', import.meta.url).href;
+    return new URL('./assets/images/cities/' + name + '.webp', import.meta.url)
+        .href;
 }
 
 export function getImageSkill(name: string) {
-    return new URL('/images/skills/' + name + '.webp', import.meta.url).href;
+    return new URL('./assets/images/skills/' + name + '.webp', import.meta.url)
+        .href;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
     base: './',
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/assets/images/cities/*.webp', // Шаблон для поиска файлов
+                    dest: 'assets/images/cities', // Целевая папка в dist
+                },
+                {
+                    src: 'src/assets/images/skills/*.webp', // Шаблон для поиска файлов
+                    dest: 'assets/images/skills', // Целевая папка в dist
+                },
+            ],
+        }),
+    ],
 });
